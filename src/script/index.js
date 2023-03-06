f9cb13ccbf1e369491766246bfb30932eda3b78c
Michael Sagel <sagel@convidera.com>
2023-03-06 11:56:34 +0100

    VUE

--- src/script/index.js
+++ src/script/index.js
@@ -1,40 +1,4 @@
-const {
-  createApp,
-  ref,
-  onMounted,
-} = Vue;
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import application from '../component/application.js'
 
-createApp({
-  setup() {
-    const list = ref(null);
-    const issueDescription = ref(null);
-    const issueButton = ref(null);
-
-    onMounted(() => {
-      const createIssueElement = (text) => {
-        const issue = document.createElement('li');
-        issue.className = 'issue';
-        issue.innerText = text;
-
-        const cancel = document.createElement('button');
-        cancel.innerText = 'Delete';
-        cancel.className = 'delete';
-        cancel.addEventListener('click', e => e.target.parentElement.remove(), {once: true});
-        issue.appendChild(cancel);
-
-        list.value.appendChild(issue);
-      };
-
-      issueButton.value.onclick = () => {
-        createIssueElement(issueDescription.value.value);
-        issueDescription.value.value = '';
-      };
-    });
-
-    return {
-      list,
-      issueDescription,
-      issueButton,
-    };
-  },
-}).mount('#app')
+createApp(application).mount('#app');
